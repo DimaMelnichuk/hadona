4b90c96ff3011b0e07d8243be3b89ee3fe499308
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-06-26 13:31:57 +0300

    Add blokc sponsor, sidebar-brand

--- app/js/components/function.js
+++ app/js/components/function.js
@@ -263,6 +263,50 @@ $(function(){
 		$(this).toggleClass('active');
 		return false;
 	});
+
+/***************** polzunok ********************/
+$(".sidebar-price__polzunok-line").slider({
+	min: 0,
+	max: 5000,
+	values: [2000, 3000],
+	range: true,
+	animate: "fast",
+	slide : function(event, ui) {    
+		 $(".sidebar-price__polzunok-input-l").val(ui.values[ 0 ]);   
+		 $(".sidebar-price__polzunok-input-r").val(ui.values[ 1 ]);  
+	}    
+});
+$(".sidebar-price__polzunok-input-l").val($(".sidebar-price__polzunok-line").slider("values", 0));
+$(".sidebar-price__polzunok-input-r").val($(".sidebar-price__polzunok-line").slider("values", 1));
+$(document).focusout(function() {
+	var input_left = $(".sidebar-price__polzunok-input-l").val().replace(/[^0-9]/g, ''),    
+	opt_left = $(".sidebar-price__polzunok-line").slider("option", "min"),
+	where_right = $(".sidebar-price__polzunok-line").slider("values", 1),
+	input_right = $(".sidebar-price__polzunok-input-r").val().replace(/[^0-9]/g, ''),    
+	opt_right = $(".sidebar-price__polzunok-line").slider("option", "max"),
+	where_left = $(".sidebar-price__polzunok-line").slider("values", 0); 
+	if (input_left > where_right) { 
+		 input_left = where_right; 
+	}
+	if (input_left < opt_left) {
+		 input_left = opt_left; 
+	}
+	if (input_left == "") {
+	input_left = 0;    
+	}        
+	if (input_right < where_left) { 
+		 input_right = where_left; 
+	}
+	if (input_right > opt_right) {
+		 input_right = opt_right; 
+	}
+	if (input_right == "") {
+	input_right = 0;    
+	}    
+	$(".sidebar-price__polzunok-input-l").val(input_left); 
+	$(".sidebar-price__polzunok-input-r").val(input_right); 
+	$(".sidebar-price__polzunok-line").slider( "values", [ input_left, input_right ] );
+});
  
 });
 
--- app/js/libs.js
+++ app/js/libs.js
@@ -1,2 +1,4 @@
 @@include('libs/jquery-3.5.1.js')
 @@include('libs/slick.js')
+@@include('libs/jquery.ui.touch-punch.min.js')
+@@include('libs/jquery-ui.min.js')
