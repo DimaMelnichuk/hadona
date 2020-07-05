100304854ed0ccb9c4047f969ce39a1f3e241d89
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-07-05 17:21:48 +0300

    Fix blocks sidebar-nav/news

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ let path = {
 		fonts: project_folder + "/fonts/",
 	},
 	src: {
-		html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+		html: [source_folder + "/**/*.html", "!" + source_folder + "/**/_*.html"],
 		css: source_folder + "/scss/style.scss",
 		js: source_folder + "/js/*.js",
 		img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp)",
