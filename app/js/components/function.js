7f41d18cdd9cb613d6a30aefeeaa19eb01215e59
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-07-07 14:49:21 +0300

    Fix page detail

--- app/js/components/function.js
+++ app/js/components/function.js
@@ -312,8 +312,6 @@ $(function(){
 	});
 
 
-
-
    $(".cart__body-shuffle").click(function(event){
 		$(this).toggleClass("active");
 	});
@@ -342,7 +340,7 @@ $(function(){
 	});
 
 	/***************** Сounter ********************/
-	$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/icons/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/icons/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+	$('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
 	$('.quantity').each(function() {
 	  var spinner = $(this),
 		 input = spinner.find('input[type="number"]'),
--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,4 @@
 @@include('components/accordion.js')
 @@include('components/rating.js')
 @@include('components/countdown.js')
+@@include('components/colors.js')
